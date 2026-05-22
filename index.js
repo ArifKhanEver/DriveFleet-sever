7eb253008b64e48db81d1798c678a9efb6232f0c
arifkhanever <mdshafiqul0404@gmail.com>
2026-05-22 10:56:57 +0600

    JWT added

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { verify } = require("node:crypto");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
 const app = express();
-app.use(cors())
-app.use(express.json())
-dotenv.config()
-const port = process.env.PORT
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,57 +18,150 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL(process.env.CLIENT_URL + "/api/auth/jwks"),
+  
+);
+
+async function verifyJWT(req, res, next) {
+  const authHeader = req?.headers?.authorization;
+
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized: Missing Header" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: Missing Token" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    // req.user = payload;
+    next();
+  } catch (error) {
+    // console.error("JWT Verification Error:", error);
+    return res
+      .status(403)
+      .json({ message: "Forbidden: Invalid Token", detail: error.message });
+  }
+}
 
 async function run() {
   try {
     await client.connect();
-    
-    const db = client.db('DriveFleet')
-    const carsCollection = db.collection('cars')
 
-    app.post('/cars', async(req, res)=>{
-        const carsData = req.body
-        console.log(carsData)
-        const result = carsCollection.insertOne(carsData)
-        res.json(result)
-    })
+    const db = client.db("DriveFleet");
+    const carsCollection = db.collection("cars");
+    const bookingCollection = db.collection("carBookings");
 
-    app.get('/cars', async(req, res)=>{
+    app.post("/cars", async (req, res) => {
+      const carsData = req.body;
+      const result = await carsCollection.insertOne(carsData);
+      res.json(result);
+    });
 
-      try{
-        const {search, type} = req.query;
+    app.get("/cars", async (req, res) => {
+      try {
+        const { search, type, userEmail } = req.query;
         let query = {};
 
-        if(search){
-          query.carName = {
-            $regex: search,
-            $options:'i'
-          }
+        if (userEmail) {
+          query.addedBy = userEmail;
         }
 
-        if(type && type !== "All"){
+        if (search) {
+          query.carName = {
+            $regex: search,
+            $options: "i",
+          };
+        }
+
+        if (type && type !== "All") {
           query.carType = type;
         }
 
-        const result = await carsCollection.find(query).toArray()
-        res.json(result)
-      }catch (error){
-        console.error('Error details:', error)
-        res.status(500).json({message:"Internal Server Error"})
+        const result = await carsCollection.find(query).toArray();
+        res.json(result);
+      } catch (error) {
+        console.error("Error details:", error);
+        res.status(500).json({ message: "Internal Server Error" });
       }
+    });
 
-    })
+    app.get("/cars/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const result = await carsCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
 
-    app.get('/cars/:id', async(req, res)=>{
-        const id = req.params.id
-        const result = await carsCollection.findOne({_id: new ObjectId(id)})
-        res.json(result)
-    })
-    
+    app.patch("/cars/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const updatedData = req.body;
+
+        const car = { _id: new ObjectId(id) };
+
+        const updateDoc = {
+          $set: {
+            dailyPrice: Number(updatedData.price),
+            description: updatedData.description,
+            availabilityStatus: updatedData.availability,
+            imageUrl: updatedData.image,
+            carType: updatedData.type,
+            pickupLocation: updatedData.location,
+          },
+        };
+
+        const result = await carsCollection.updateOne(car, updateDoc);
+        res.json(result);
+      } catch (error) {
+        console.error("Backend Patch Error:", error);
+        res
+          .status(500)
+          .send({ error: true, message: "Server error during patch update" });
+      }
+    });
+
+    app.delete("/cars/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await carsCollection.deleteOne({ _id: new ObjectId(id) });
+    });
+
+    // Bookings here
+    app.post("/bookings", async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCollection.insertOne(bookingData);
+      res.json(result);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      try {
+        const { userEmail } = req.query;
+        const result = await bookingCollection.find({ userEmail }).toArray();
+        res.json(result);
+      } catch (error) {
+        console.error("Error Details:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await bookingCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
+
     await client.db("DriveFleet").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -75,9 +169,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
